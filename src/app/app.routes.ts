75dd08d9767382a9b2e60e94e7f574a0dc254413
JamisonCleveland <43040811+JamisonCleveland@users.noreply.github.com>
2024-09-28 19:51:13 -0400

    project setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PersonalDetailsComponent } from './personal-details/personal-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'personal-details', component: PersonalDetailsComponent },
+    { path: 'work-experience', component: PersonalDetailsComponent },
+    { path: 'education', component: PersonalDetailsComponent },
+    { path: 'skills', component: PersonalDetailsComponent },
+    { path: '',   redirectTo: '/personal-details', pathMatch: 'full' },
+];
