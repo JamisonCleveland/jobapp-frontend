c4d9700281af487f3f4080fc438b6f66880e07e8
JamisonCleveland <43040811+JamisonCleveland@users.noreply.github.com>
2024-09-29 09:12:22 -0400

    basic personal-details form

--- src/app/personal-details/personal-details.component.ts
+++ src/app/personal-details/personal-details.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-personal-details',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './personal-details.component.html',
   styleUrl: './personal-details.component.scss'
 })
 export class PersonalDetailsComponent {
-
+  formBuilder = inject(FormBuilder);
+  personalDetailsForm = this.formBuilder.group({
+    fullName: ['', Validators.required],
+    emailAddress: ['', Validators.required, Validators.email],
+    phoneNumber: ['', Validators.required],
+    address: [''],
+    linkedinProfileUrl: [''],
+  });
 }
