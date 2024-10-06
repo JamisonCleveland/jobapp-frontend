5ad42bbc5b0be566c638c1362fa08dd16c02ea94
JamisonCleveland <43040811+JamisonCleveland@users.noreply.github.com>
2024-10-06 08:40:40 -0400

    added model

---
+++ src/app/job-application.ts
@@ -0,0 +1,8 @@
+export interface JobApplication {
+  // personal details
+  fullName: string,
+  emailAddress: string,
+  phoneNumber: string,
+  address?: string,
+  linkedinProfileUrl?: string,
+}
