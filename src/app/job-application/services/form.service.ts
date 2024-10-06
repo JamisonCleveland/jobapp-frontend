20072735ea09b0d10d20b85c883e8ffec38310dc
JamisonCleveland <43040811+JamisonCleveland@users.noreply.github.com>
2024-10-06 12:21:45 -0400

    cleanedup form service

--- src/app/job-application/services/form.service.ts
+++ src/app/job-application/services/form.service.ts
@@ -1,8 +1,20 @@
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { inject, Injectable } from '@angular/core';
 
 const phoneNumberPattern = /^[- +()0-9]+$/;
 const linkedinProfileUrlPattern = /^(https?:\/\/)?(www\.)?linkedin\.com\/in\/[A-z0-9_-]+\/?$/;
+
+export interface PersonalDetailsForm extends FormGroup<{
+  fullName: FormControl<string>,
+  emailAddress: FormControl<string>,
+  phoneNumber: FormControl<string>,
+  address: FormControl<string | null>,
+  linkedinProfileUrl: FormControl<string | null>,
+}> { }
+
+export interface Form extends FormGroup<{
+  personalDetails: PersonalDetailsForm,
+}> { }
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +22,23 @@ const linkedinProfileUrlPattern = /^(https?:\/\/)?(www\.)?linkedin\.com\/in\/[A-
 export class FormService {
   private _formBuilder = inject(FormBuilder);
 
-  personalDetailsForm = this._formBuilder.nonNullable.group({
-    fullName: ['', Validators.required],
-    emailAddress: ['', [Validators.required, Validators.email]],
-    phoneNumber: ['', [Validators.required, Validators.pattern(phoneNumberPattern)]],
-    address: [''],
-    linkedinProfileUrl: ['', Validators.pattern(linkedinProfileUrlPattern)],
+  personalDetailsForm = this._formBuilder.group({
+    fullName: ['', {
+      nonNullable: true,
+      validators: Validators.required
+    }],
+    emailAddress: ['', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email]
+    }],
+    phoneNumber: ['', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.pattern(phoneNumberPattern)]
+    }],
+    address: [null],
+    linkedinProfileUrl: [null, {
+      validators: Validators.pattern(linkedinProfileUrlPattern)
+    }],
   });
 
   form = this._formBuilder.group({
