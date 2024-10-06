f5157a5123eac350e2e0acd21b5ed2a3eed01701
JamisonCleveland <43040811+JamisonCleveland@users.noreply.github.com>
2024-10-06 12:05:35 -0400

    wrapping w/ main

--- src/app/job-application/pages/personal-details/personal-details.component.ts
+++ src/app/job-application/pages/personal-details/personal-details.component.ts
@@ -1,13 +1,5 @@
-import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
 import { FormService } from '../../services/form.service';
-import { DebugFormStatusComponent } from '../../components/form-debug/form-debug.component';
-import { NoopAnimationPlayer } from '@angular/animations';
-import { AnimationDriver } from '@angular/animations/browser';
 
 export interface PersonalDetails {
   fullName: string,
@@ -17,34 +9,35 @@ export interface PersonalDetails {
   linkedinProfileUrl: string,
 }
 
-
 @Component({
   selector: 'app-personal-details',
   template: `
-    <form [formGroup]="personalDetailsForm">
-      <mat-form-field>
-        <mat-label>Full Name</mat-label>
-        <input matInput type="text" formControlName="fullName">
-      </mat-form-field>
-      <mat-form-field>
-        <mat-label>Email Address</mat-label>
-        <input matInput type="email" formControlName="emailAddress">
-      </mat-form-field>
-      <mat-form-field>
-        <mat-label>Phone Number</mat-label>
-        <input matInput type="tel" formControlName="phoneNumber">
-        <mat-icon matPrefix>phone</mat-icon>
-      </mat-form-field>
-      <mat-form-field>
-        <mat-label>Address</mat-label>
-        <input matInput type="text" formControlName="address">
-      </mat-form-field>
-      <mat-form-field>
-        <mat-label>Linkedin Profile</mat-label>
-        <input matInput type="url" formControlName="linkedinProfileUrl">
-      </mat-form-field>
-    </form>
-    <debug-form-status [form]="personalDetailsForm"></debug-form-status>
+    <main>
+      <form [formGroup]="personalDetailsForm">
+        <mat-form-field>
+          <mat-label>Full Name</mat-label>
+          <input matInput type="text" formControlName="fullName">
+        </mat-form-field>
+        <mat-form-field>
+          <mat-label>Email Address</mat-label>
+          <input matInput type="email" formControlName="emailAddress">
+        </mat-form-field>
+        <mat-form-field>
+          <mat-label>Phone Number</mat-label>
+          <input matInput type="tel" formControlName="phoneNumber">
+          <mat-icon matPrefix>phone</mat-icon>
+        </mat-form-field>
+        <mat-form-field>
+          <mat-label>Address</mat-label>
+          <input matInput type="text" formControlName="address">
+        </mat-form-field>
+        <mat-form-field>
+          <mat-label>Linkedin Profile</mat-label>
+          <input matInput type="url" formControlName="linkedinProfileUrl">
+        </mat-form-field>
+      </form>
+      <debug-form-status [form]="personalDetailsForm"></debug-form-status>
+    </main>
   `,
   styleUrl: './personal-details.component.scss'
 })
